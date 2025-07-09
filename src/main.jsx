19175b5258a4c619ea5f28d6b82f46ac2082c2fc
MojolaoluwaGafar <mojolaoluwa1212@gmail.com>
2025-07-09 01:37:08 +0100

    test deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx'
 import AuthProvider from "./context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={clientId}>
       <AuthProvider>
         <App />
       </AuthProvider>
